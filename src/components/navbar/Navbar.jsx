53ae6bafd1fcff58d131c6fb64586d9d60114360
Mamadou mbaye wade <mamadou.mb.wade@gmail.com>
2022-01-07 16:59:43 +0000

    second commit to integer a list and listItem

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,14 @@
 import { ArrowDropDown, Notifications, Search } from "@mui/icons-material";
+import { useState } from "react";
 import "./navbar.scss";
 const Navbar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+  window.onscroll = () => {
+    setIsScrolled(window.pageYOffset === 0 ? false : true);
+    return () => (window.onscroll = null);
+  };
   return (
-    <div className="navbar">
+    <div className={isScrolled ? "navbar scrolled" : "navbar"}>
       <div className="container">
         <div className="left">
           <img
@@ -16,14 +22,20 @@ const Navbar = () => {
           <span>My List</span>
         </div>
         <div className="right">
-          <Search />
+          <Search className="icon" />
           <span>KID</span>
-          <Notifications />
+          <Notifications className="icon" />
           <img
             src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
             alt=""
           />
-          <ArrowDropDown />
+          <div className="profile">
+            <ArrowDropDown className="icon" />
+            <div className="options">
+              <span>Settings</span>
+              <span>Logo</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
